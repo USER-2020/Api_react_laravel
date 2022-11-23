378e4e3482545b0e34ff7882a901342773ee0e79
USER 2020 <hackingetico2021@gmail.com>
2022-11-23 13:09:36 -0500

    Prueba

---
+++ PUNTO_2/database/seeders/DatabaseSeeder.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
+
+class DatabaseSeeder extends Seeder
+{
+    /**
+     * Seed the application's database.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        // \App\Models\User::factory(10)->create();
+        // DB::table('asignaturas')->insert([
+        //     'nombre' => 'matematicas II',
+        //     'cant_creditos' => '3',
+        //     'intens_horaria' => '4',
+        //     'carr_asociada' => 'ASI, FISICA, ING'
+        // ]),
+
+        // DB::table('asignaturas')->insert([
+        //     'nombre' => 'matematicas II',
+        //     'cant_creditos' => '3',
+        //     'intens_horaria' => '4',
+        //     'carr_asociada' => 'ASI, FISICA, ING'
+        // ]),
+    }
+}
